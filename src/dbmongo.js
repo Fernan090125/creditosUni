957076391276c63c121937061a4beb9f708430df
Fernan090125 <fernanlambrano09@gmail.com>
2021-12-03 14:15:34 -0500

    credito

--- src/dbmongo.js
+++ src/dbmongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-const mongose_uri = 'mongodb://localhost:27017/credito';
-//const mongose_uri = mongodb+srv://root:1005661451@dialogflowcluster.fsiuc.mongodb.net/Credito?retryWrites=true&w=majority'
+//const mongose_uri = 'mongodb://localhost:27017/credito';
+const mongose_uri = 'mongodb+srv://root:1005661451@dialogflowcluster.fsiuc.mongodb.net/Credito?retryWrites=true&w=majority'
 
 mongoose.connect(mongose_uri,{
     useNewUrlParser: true,  
