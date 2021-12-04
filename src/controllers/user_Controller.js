a9fe52bb2e91a080ec9e576a14b8d25611d72c93
Fernan090125 <fernanlambrano09@gmail.com>
2021-12-04 15:25:36 -0500

    post

--- src/controllers/user_Controller.js
+++ src/controllers/user_Controller.js
@@ -1,57 +1,64 @@
-const User = require('../models/userModel');
-const Credito = require('../models/creditModel');
+const User = require("../models/userModel");
+const Credito = require("../models/creditModel");
 
-const userController = {}
+const userController = {};
 
 userController.getUserInfo = async (req, res) => {
-    const { id } = req.params;
-    const user = await User.findById(id);
-    const credit = await Credito.findOne({user});
-    console.log(credit);
-    res.json({
-        User: user,
-        Credit: credit
-    });
-}
+  const { id } = req.params;
+  const user = await User.findById(id);
+  const credit = await Credito.findOne({ user });
+  console.log(credit);
+  res.json({
+    User: user,
+    Credit: credit,
+  });
+};
 
 userController.PostUser = async (req, res) => {
-    const user = new User(req.body);
-    await user.save();
-    res.json({
-        status: 'User Saved'
+  const user = new User(req.body);
+  const is = await User.findOne({ email: req.body.email });
+  if (is) {
+    res.status(400).json({
+      message: "El usuario ya existe",
     });
-}
+  } else {
+    await user.save();
+    res.status(200).json({
+      status: "User Saved",
+    });
+  }
+};
 
 userController.Login = async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email});
-    if (!user) {
-        res.json({
-            status: 'User not found'
-        });
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    res.status(404).json({
+      status: "User not found",
+    });
+  } else {
+    if (user.password === password) {
+      res.status(200).json({
+        role: user.rol,
+      });
     } else {
-        if (user.password === password) {
-            res.status(200).json({
-                role: user.rol,
-            });
-        }else{
-            res.json({
-                status: 'Password incorrect'
-            });
-        }
+      res.status(404).json({
+        status: "Password incorrect",
+      });
     }
-}
+  }
+};
 
 userController.updateUser = async (req, res) => {
-    const { id } = req.params;
-    const user = {
-        nombre: req.body.nombre,
-        apellido: req.body.apellido,
-    }
-    await User.findByIdAndUpdate(id, {$set: user}, {new: true});
-    res.json({
-        status: 'User Updated'
-    });
-}
+  const { id } = req.params;
+  const user = {
+    nombre: req.body.nombre,
+    apellido: req.body.apellido,
+  };
+  await User.findByIdAndUpdate(id, { $set: user }, { new: true });
+  res.json({
+    status: "User Updated",
+  });
+};
 
 module.exports = userController;
